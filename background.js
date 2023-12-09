5950723453879ac92435c9d41e921b2472e1d470
Lemio <kota.shimizu.biz@gmail.com>
2023-12-10 02:59:29 +0900

    完成

--- background.js
+++ background.js
@@ -22,6 +22,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 title: 'Retrieved Data',
                 message: 'Your message here'
               });
+            addRecordToNotionDatabase(data);
           });
         }
       );
@@ -40,7 +41,7 @@ async function callOpenAI(apiKey, text) {
     body: JSON.stringify({
       model: 'gpt-3.5-turbo-1106',
       messages: [
-        {"role": "system", "content":"以下の文章を要約してください"},
+        {"role": "system", "content":"以下の文章を要約して日本語にしてください"},
         {"role": "user", "content": text}],
       max_tokens: 1000,
       temperature: 0
@@ -48,4 +49,51 @@ async function callOpenAI(apiKey, text) {
   });
   let summary = await response.json();
   return summary;
+}
+async function addRecordToNotionDatabase(data) {
+  chrome.storage.sync.get(['secretKey', 'databaseId'], async function(notionResult) {
+    let secretKey = notionResult.secretKey;
+    let databaseId = notionResult.databaseId;
+    let parsedData = JSON.parse(data);
+
+    if (!parsedData.title) {
+      console.error('Title is missing in the data object');
+      return;
+    }
+
+    let text = parsedData.text;
+    if (text && text.length > 2000) {
+      text = text.substring(0, 2000);  // Keep only the first 2000 characters
+    }
+
+    let response = await fetch(`https://api.notion.com/v1/pages`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${secretKey}`,
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        parent: { database_id: databaseId },
+        properties: {
+          タイトル: { title: [{ text: { content: parsedData.title } }] },
+          作成日: { date: { start: parsedData.timestamp, end: null } },
+          要約内容: { rich_text: [{ text: { content: parsedData.summary } }] },
+          URL: { url: parsedData.url },
+          // テキスト: { rich_text: [{ text: { content: text } }] },
+          セレクト: { select: { name: '未読' } }  // Replace 'Your Select Value' with the actual value
+        }
+      })
+    });
+
+    let result = await response.json();
+    console.log(result);
+    alert('登録が完了しました');
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icon.png',
+      title: '通知',
+      message: '登録が完了しました'
+    });
+  });
 }
